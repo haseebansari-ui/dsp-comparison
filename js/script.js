b69acf9f388458194660ac17cd862991ee3d64c5
haseebansari-ui <haseeb.ansari@havmormedia.com>
2026-05-12 16:37:23 +0530

    working on index page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 "use strict"
 
+// accordian
+
 const accordian = document.querySelectorAll('.contentBx');
 
 for (let i = 0; i < accordian.length; i++) {
@@ -7,3 +9,37 @@ for (let i = 0; i < accordian.length; i++) {
 		this.classList.toggle('active');
 	});
 }
+
+
+// TABBER JS
+
+const tabBtns = document.querySelectorAll('.tab-btn');
+const tabContents = document.querySelectorAll('.tab-content');
+
+tabBtns.forEach(btn => {
+
+    btn.addEventListener('click', () => {
+
+        const target = btn.dataset.tab;
+
+        // REMOVE ACTIVE
+
+        tabBtns.forEach(button => {
+            button.classList.remove('active');
+        });
+
+        tabContents.forEach(content => {
+            content.classList.remove('active');
+        });
+
+        // ADD ACTIVE
+
+        btn.classList.add('active');
+
+        document
+            .getElementById(target)
+            .classList.add('active');
+
+    });
+
+});
