4766e5ed847c1268d996651932291583bde7f0e6
haseebansari-ui <haseeb.ansari@havmormedia.com>
2026-05-18 15:57:06 +0530

    change header in all pages

--- js/script.js
+++ js/script.js
@@ -45,3 +45,16 @@ tabBtns.forEach(btn => {
 });
 
 
+const menuBarOpen = document.querySelector(".other-nav-link ul");
+const menuToggle = document.querySelector(".menue-bar-bx");
+
+menuToggle.addEventListener("click", () => {
+    menuBarOpen.classList.toggle("active");
+});
+
+const innerMenuBarOpen = document.querySelector(".other-nav-link ul .drop-down-bx .drop-down-menue");
+const innerMenuToggle = document.querySelector(".inner-menue");
+
+innerMenuToggle.addEventListener("click", () => {
+    innerMenuBarOpen.classList.toggle("active");
+});
