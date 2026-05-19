5749d70ad90d198a80e71c2db0c126540ea10d15
haseebansari-ui <haseeb.ansari@havmormedia.com>
2026-05-19 13:44:50 +0530

    add toggle menue icons in header section

--- js/script.js
+++ js/script.js
@@ -50,6 +50,7 @@ const menuToggle = document.querySelector(".menue-bar-bx");
 
 menuToggle.addEventListener("click", () => {
     menuBarOpen.classList.toggle("active");
+    menuToggle.classList.toggle("active");
 });
 
 const innerMenuBarOpen = document.querySelector(".other-nav-link ul .drop-down-bx .drop-down-menue");
